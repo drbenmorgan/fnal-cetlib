851508343eaba487176ace73e00a8f6ec416688f
Ben Morgan <Ben.Morgan@warwick.ac.uk>
2016-01-30 17:25:47 +0000

    Import CetBuildTools for testing
    To get tests working, vendor in minimal set of CetBuildTools modules
    needed to run cet_test. Remove find_ups_boost dependence because
    we now find Boost by normal means. The aim here is to find the
    minimal set of CetBuildTools functionality that makes sense,
    and to refactor any cetlib related modules into cetlib itself to
    remove potential circular dependencies. This will also help
    to identify UPS/FNAL and GNU/Linux lockdowns in cetbuildtools in a
    "production" style package.

---
+++ cbt/CetGetProductInfo.cmake
@@ -0,0 +1,20 @@
+#-----------------------------------------------------------------------
+# Modifications Copyright 2015 Ben Morgan <Ben.Morgan@warwick.ac.uk
+# Modifications Copyright 2015 University of Warwick
+
+set(GET_PRODUCT_INFO ${cetbuildtools_BINDIR}/report_product_info)
+message(STATUS "${GET_PRODUCT_INFO}")
+
+function(cet_get_product_info_item ITEM OUTPUT_VAR)
+  execute_process(COMMAND ${GET_PRODUCT_INFO}
+    ${CMAKE_CURRENT_BINARY_DIR}
+    ${ITEM}
+    OUTPUT_VARIABLE output
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+    RESULT_VARIABLE ec
+    )
+  set(${OUTPUT_VAR} ${output} PARENT_SCOPE)
+  if(ARGV2)
+    set(${ARGV2} ${ec} PARENT_SCOPE)
+  endif()
+endfunction()
