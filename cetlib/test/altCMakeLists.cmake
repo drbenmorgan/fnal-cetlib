6607ad7f19d4aedba3c6a00060e2119842cc5b5c
Ben Morgan <Ben.Morgan@warwick.ac.uk>
2017-02-17 15:43:09 +0000

    Add issue-11678 tests to altCMake script
    Kept in separate commit to allow easy upstream merge

--- cetlib/test/altCMakeLists.cmake
+++ cetlib/test/altCMakeLists.cmake
@@ -113,9 +113,18 @@ cet_make_library(LIBRARY_NAME cetlib_test_TestPluginBase SOURCE TestPluginBase.c
 include(BasicPlugin)
 basic_plugin(TestPlugin "plugin" NO_INSTALL cetlib_test_TestPluginBase)
 
+# Use default Plugin lookup
 cet_test(PluginFactory_t USE_BOOST_UNIT
   LIBRARIES cetlib cetlib_test_TestPluginBase
   )
+
+# Use custom Plugin lookup
+cet_test(PluginFactoryCustomSearchPath_t USE_BOOST_UNIT
+  SOURCES PluginFactory_t.cc
+  LIBRARIES cetlib cetlib_test_TestPluginBase
+  TEST_PROPERTIES ENVIRONMENT PLUGIN_FACTORY_SEARCH_PATH=$<TARGET_FILE_DIR:cetlib_test_TestPlugin_plugin>
+  )
+target_compile_definitions(PluginFactoryCustomSearchPath_t PRIVATE PLUGIN_FACTORY_SEARCH_PATH=1)
 
 function(test_library LIBSPEC)
   add_custom_command(OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${LIBSPEC}.cc
@@ -136,11 +145,22 @@ test_library(1_1_3)
 test_library(1_2_3)
 test_library(2_1_5)
 
+# Use default library search path
 cet_test(LibraryManager_t USE_BOOST_UNIT
   LIBRARIES
   ${Boost_FILESYSTEM_LIBRARY}
   ${CMAKE_DL_LIBS}
   )
+
+# Use custom library search path
+cet_test(LibraryManagerCustomSearchPath_t USE_BOOST_UNIT
+  SOURCES LibraryManager_t.cc
+  LIBRARIES
+  ${Boost_FILESYSTEM_LIBRARY}
+  ${CMAKE_DL_LIBS}
+  TEST_PROPERTIES ENVIRONMENT LIBRARY_MANAGER_SEARCH_PATH=$<TARGET_FILE_DIR:1_1_1_cetlibtest>
+  )
+target_compile_definitions(LibraryManagerCustomSearchPath_t PRIVATE LIBRARY_MANAGER_SEARCH_PATH=1)
 
 cet_test(replace_all_test USE_BOOST_UNIT
   LIBRARIES
